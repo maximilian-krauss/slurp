6e8e3a86a79f8124b68e0042a65f0cd65e186505
maximilian-krauss <maximilian.krauss@covus.de>
2014-06-27 16:59:59 +0200

    More initialization stuff

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp  				= require('gulp'),
 		iced					= require('gulp-iced'),
 		concat				= require('gulp-concat'),
 		clean					= require('gulp-clean'),
+    stylus        = require('gulp-stylus'),
 		thirdPartyJs	= [
 
 		],
@@ -16,7 +17,8 @@ var gulp  				= require('gulp'),
 			client: {
 				iced: { src: ['./src/client/app/app.iced', './src/client/app/**/*.iced'], dest: './public/js' },
 				styles: { src: './src/client/styles/*.styl', dest: './public/css' },
-				images: { src: './src/client/images/**/*', dest: './public/images' }
+				images: { src: './src/client/images/**/*', dest: './public/images' },
+        html: { src: '/src/client/html/**/*.html', dest: './public/html' }
 			}
 		};
 
@@ -37,6 +39,9 @@ gulp.task('clean:client', function() {
 
 	gulp.src(directories.client.images.dest)
 		.pipe(clean({ read: false, force: true }));
+
+  gulp.src(directories.client.html.dest)
+    .pipe(clean({ read: false, force: true }));
 });
 
 // Server
@@ -46,10 +51,23 @@ gulp.task('server:compile', function() {
 		.pipe(gulp.dest(directories.server.iced.dest));
 });
 
-
 // Client
 gulp.task('client:compile', function() {
+  gulp.src(directories.client.iced.src)
+    .pipe(iced({ bare: false, runtime: 'window' }))
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest(directories.client.iced.dest));
 
+  gulp.src(directories.client.styles.src)
+    .pipe(stylus({set: ['compress']}))
+    .pipe(concat('app.css'))
+    .pipe(gulp.dest(directories.client.styles.dest));
+
+  gulp.src(directories.client.html.src)
+    .pipe(gulp.dest(directories.client.html.dest));
+
+  gulp.src(directories.client.images.src)
+    .pipe(gulp.dest(directories.client.styles.dest));
 });
 
 
